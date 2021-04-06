1a6bb39dc369bb9e4090ec4e3f2c56c1ec18fd27
AdrianoAntoniev <adriano.antoniev@outlook.com>
2021-04-06 20:18:17 -0300

    Print result of request for testing purposes.

--- Apply-Stefanini_Raizen/Workers/FetchingData.swift
+++ Apply-Stefanini_Raizen/Workers/FetchingData.swift
@@ -7,9 +7,18 @@
 
 import Foundation
 import SwiftUI
-
+import Alamofire
 
 class FetchingData {
     
+    init() {
+        let headers: HTTPHeaders = [.contentType("application/json"),
+                                        .authorization("Client-ID 0093d54ab7d0851")]
+        let request = AF.request("https://api.imgur.com/3/gallery/hot/viral/day/0", headers: headers)
+        
+        request.responseJSON { response in
+            print(response.result)
+        }
+    }
     
 }
